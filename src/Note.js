eef7bbe5e0e75d79cdfc2571e94fc3d64c4094c5
Juan Allo Ron <2070972+juanallo@users.noreply.github.com>
2019-03-15 10:53:32 -0700

    adding note edit

--- src/Note.js
+++ src/Note.js
@@ -1,18 +1,67 @@
 import React, {Component} from 'react';
-import {FaPen, FaTrash} from 'react-icons/fa';
+import {FaPen, FaTrash, FaRegSave} from 'react-icons/fa';
 
 class Note extends Component {
 
-	render(){
+	constructor({text='Learn React'}){
+		super({text});
+
+		this.state = {
+			editing: false,
+			text: text
+		};
+
+		this.remove = this.remove.bind(this);
+		this.edit = this.edit.bind(this);
+		this.renderForm = this.renderForm.bind(this);
+		this.renderDisplay = this.renderDisplay.bind(this);
+		this.save = this.save.bind(this);
+	}
+
+	edit(){
+		this.setState({
+			editing: true
+		})
+	}
+
+	save(){
+		this.setState({
+			editing: false,
+			text: this._nexText.value
+		});
+	}
+
+	remove(){
+		alert('Remove');
+	}
+
+	renderForm() {
 		return (
 			<div className="note">
-				<p>Learn React</p>
+				<form className="note__form">
+					<textarea ref={ input => this._nexText = input} defaultValue={this.state.text}/>
+					<div className="form__footer">
+						<button id="save" onClick={this.save}><FaRegSave /></button>
+					</div>
+				</form>
+			</div>
+		);
+	}
+
+	renderDisplay(){
+		return (
+			<div className="note">
+				<p>{this.state.text}</p>
 				<span>
-					<button><FaPen id="edit" /></button>
-					<button><FaTrash id="remove"/></button>
+					<button id="edit" onClick={this.edit}><FaPen  /></button>
+					<button id="remove" onClick={this.remove}><FaTrash /></button>
 				</span>
 			</div>
 		);
+	}
+
+	render(){
+		return this.state.editing ? this.renderForm() : this.renderDisplay();
 	}
 }
 
