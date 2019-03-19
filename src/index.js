97d6f65a3ea2e836e50856a43cd0cc1a31a754e8
Juan Allo Ron <2070972+juanallo@users.noreply.github.com>
2019-03-18 20:33:27 -0700

    moving components to a component folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Board from './Board';
+import Board from './components/Board';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Board boardName="bulletin-board-notes"/>, document.getElementById('root'));
